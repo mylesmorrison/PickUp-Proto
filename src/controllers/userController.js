d1efb10d9f9af2e4fec65a8bb86432bf35c7c2e6
mylesmorrison <mylesmorrison@me.com>
2025-02-13 12:52:47 +1100

    added a sample .env file and npm dependencies

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,10 @@
 
 const getUserProfile = (req, res) => {
-
+    const userID = req.params.id;
+    res.json()
 }
 
 const getUserEvents = (req, res) => {
-
+    
 }
 
---
+++ src/db/dbConnection.js
@@ -0,0 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
