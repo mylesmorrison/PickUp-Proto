97f5dddabb035550fa13009a653381b469f629e6
mylesmorrison <mylesmorrison@me.com>
2025-02-14 09:35:19 +1100

    started working on sandbox files just to test functionality for myself. dbConnection file added.

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -2,3 +2,17 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+async function connectDB() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/testdb", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected!");
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
+  }
+}
+
+connectDB();
+
