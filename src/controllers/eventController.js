573b9830e01ed7553e1e61420d955ce457ac75e2
mylesmorrison <mylesmorrison@me.com>
2025-03-10 08:08:23 +1100

    added comments and validation to eventController.js

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,5 +1,12 @@
+// import database functions 
 const { getEvents, addEvent } = require("../db/eventDb")
 
+/**
+ * Fetch all events from db then return them as JSON.
+ * @route GET /events.
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Expres response object.
+ */
 const getAllEvents = async (req, res) => {
     try {
         const events = await getEvents()
@@ -13,9 +20,20 @@ const getEventInformation = (req, res) => {
     
 }
 
+/**
+ * addEvent to the database and then send response message.
+ * @route POST /events/create
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
 const createEvent = async (req, res) => {
     try {
         const data = req.body
+        //validate inputs 
+
+        if (!data.event_name || !data.event_date || !data.event_location || data.event_desc) {
+            return res.status(400).json({ error: "All fields are required: event_name, event_date, event_location, event_desc" })
+        }
         const event = await addEvent(data.event_name, data.event_date, data.event_location, data.event_desc)
         console.log(event)
         res.json({"response": "successful"})
@@ -39,6 +57,8 @@ const joinEvent = (req, res) => {
 const leaveEvent = (req, res) => {
     
 }
+
+
 
 
 module.exports = { getAllEvents, createEvent }
